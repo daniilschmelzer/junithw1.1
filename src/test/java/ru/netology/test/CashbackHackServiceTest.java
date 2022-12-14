9d44ef3613150b43fcd9cb0fd5c2a7996d9a2f00
daniilschmelzer <daniilschmelzer@gmail.com>
2022-12-14 18:19:01 +0300

    added branch testng

---
+++ src/test/java/ru/netology/test/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void cashbackHackServiceTest1() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void cashbackHackServiceTest2() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = cashbackHackService.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void cashbackHackServiceTest3() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = cashbackHackService.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+}
