5bf52e2df6bfa8ee0c6980af83b939c02e29681c
daniilschmelzer <daniilschmelzer@gmail.com>
2022-12-14 18:21:59 +0300

    added branch junit4

--- src/test/java/ru/netology/test/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/test/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.test;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import ru.netology.service.CashbackHackService;
 
 public class CashbackHackServiceTest {
